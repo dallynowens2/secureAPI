4b916436b5fb078a913369392c6dc2b5169ecfef
dallynowens2 <dallynowens2@gmail.com>
2022-02-28 13:47:58 -0700

    added status

--- secureAPI/Controllers/StatusController.cs
+++ secureAPI/Controllers/StatusController.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace secureAPI.Controllers
 {
-    public class StatusController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController: ControllerBase
     {
+
+        [HttpGet]
+        public async Task<ActionResult<string>> GetUserInfo()
+        {
+            string output = await $"systemctl status wg-quick@wg0.service".Bash();
+            return output;
+        }
     }
 }
---
+++ secureAPI/bash_helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace secureAPI
+{
+    public static class cmdhelper
+    {
+        public static Task<string> Bash(this string cmd)
+        {
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "bash",
+                    Arguments = $"-c \"{cmd}\"",
+                    UserName = "root",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return Task.FromResult(result);
+        }
+    }
+}
