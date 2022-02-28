ef8e4799005b1f393ef19dadd8e02dee49ebe75d
dallynowens2 <dallynowens2@gmail.com>
2022-02-28 01:26:22 -0700

    adding status

---
+++ secureAPI/Models/StatusInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace secureAPI.Models
+{
+    public class StatusInfo
+    {
+       public string status { get; set; }
+    }
+}
